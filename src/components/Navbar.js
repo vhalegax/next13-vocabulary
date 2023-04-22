62a8b3b8cccdfc499fdb628246d17cebdac3e2f7
wisnu setyawan <stephanus.ws@gmail.com>
2023-04-22 14:12:58 +0700

    change: tailwind clas order

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 const Navbar = () => {
   return (
     <>
-      <div className="h-14 fixed top-0 bg-primary text-primary-content w-screen p-3 border-b-primary-content border-solid border-b-2">
+      <div className="fixed top-0 h-14 w-screen border-b-2 border-solid border-b-primary-content bg-primary p-3 text-primary-content">
         <div className="container mx-auto flex items-center justify-between">
           <div>
-            <h1 className="font-medium text-lg">Vocabulary</h1>
+            <h1 className="text-lg font-medium">Vocabulary</h1>
           </div>
 
           <div></div>
