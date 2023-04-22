306d19b624db6cb274d0a7084f89f33340d2087d
wisnu setyawan <stephanus.ws@gmail.com>
2023-04-22 15:25:21 +0700

    change: form style

--- src/components/ModalAppend.js
+++ src/components/ModalAppend.js
@@ -50,136 +50,137 @@ const ModalAppend = (props) => {
   }
 
   return (
-    <div className="fixed z-10 inset-0 bg-gray-900 bg-opacity-30 flex justify-center items-center">
+    <div className="fixed inset-0 z-10 flex min-h-screen items-center justify-center bg-black bg-opacity-60">
       {/* Modal Content */}
-      <div className="bg-white rounded-lg">
-        {/* <!-- Modal header --> */}
-        <div className="flex justify-between p-4">
-          <h2 className="text-lg font-semibold text-gray-800">Modal Title</h2>
+      <div className="">
+        <div className="bg-base-200">
+          {/* <!-- Modal header --> */}
+          <div className="flex justify-between bg-primary p-4 text-primary-content">
+            <h2 className="text-lg font-semibold capitalize">Add vocabulary</h2>
 
-          <button
-            onClick={onCloseModal}
-            className="text-gray-400 hover:text-gray-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-
-        <hr className="border-b border-solid border-b-black"></hr>
-
-        <div className="w-96 p-4">
-          <form onSubmit={handleSubmit}>
-            <div className="my-3">
-              <label className="font-medium text-sm capitalize">
-                indonesia
-              </label>
-
-              <br />
-
-              <input
-                name="indonesia"
-                type="text"
-                className="border rounded p-2 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-                placeholder="enter text here..."
-                value={form.indonesia}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="font-medium text-sm capitalize">
-                base form
-              </label>
-              <br />
-              <input
-                name="baseForm"
-                type="text"
-                className="border rounded p-2 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-                placeholder="enter text here..."
-                value={form.baseForm}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="font-medium text-sm capitalize">present</label>
-              <br />
-              <input
-                name="present"
-                type="text"
-                className="border rounded p-2 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-                placeholder="enter text here..."
-                value={form.present}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="font-medium text-sm capitalize">
-                present participle
-              </label>
-              <br />
-              <input
-                name="presentParticiple"
-                type="text"
-                className="border rounded p-2 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-                placeholder="enter text here..."
-                value={form.presentParticiple}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="font-medium text-sm capitalize">past</label>
-              <br />
-              <input
-                name="past"
-                type="text"
-                className="border rounded p-2 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-                placeholder="enter text here..."
-                value={form.past}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="my-3">
-              <label className="font-medium text-sm capitalize">
-                past participle
-              </label>
-              <br />
-              <input
-                name="pastParticiple"
-                type="text"
-                className="border rounded p-2 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-                placeholder="enter text here..."
-                value={form.pastParticiple}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="mt-4 flex justify-end">
-              <button
-                disabled={isLoading}
-                className="bg-blue-500 p-2 rounded-lg text-sm text-white"
-                type="submit"
+            <button onClick={onCloseModal}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                {isLoading ? 'Loading...' : 'Submit'}
-              </button>
-            </div>
-          </form>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <hr className="border-b border-solid border-b-primary-content"></hr>
+
+          <div className="w-96 p-4">
+            <form onSubmit={handleSubmit}>
+              <div className="my-3">
+                <label className="text-sm capitalize text-primary">
+                  indonesia
+                </label>
+
+                <br />
+
+                <input
+                  name="indonesia"
+                  type="text"
+                  className="focus:shadow-outline-primary block w-full rounded border border-primary bg-base-100 p-2 capitalize focus:border-primary-focus focus:outline-none"
+                  placeholder="enter text here..."
+                  value={form.indonesia}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="my-3">
+                <label className="text-sm capitalize text-primary">
+                  base form
+                </label>
+                <br />
+                <input
+                  name="baseForm"
+                  type="text"
+                  className="focus:shadow-outline-primary block w-full rounded border border-primary bg-base-100 p-2 capitalize focus:border-primary-focus focus:outline-none"
+                  placeholder="enter text here..."
+                  value={form.baseForm}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="my-3">
+                <label className="text-sm capitalize text-primary">
+                  present
+                </label>
+                <br />
+                <input
+                  name="present"
+                  type="text"
+                  className="focus:shadow-outline-primary block w-full rounded border border-primary bg-base-100 p-2 capitalize focus:border-primary-focus focus:outline-none"
+                  placeholder="enter text here..."
+                  value={form.present}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="my-3">
+                <label className="text-sm capitalize text-primary">
+                  present participle
+                </label>
+                <br />
+                <input
+                  name="presentParticiple"
+                  type="text"
+                  className="focus:shadow-outline-primary block w-full rounded border border-primary bg-base-100 p-2 capitalize focus:border-primary-focus focus:outline-none"
+                  placeholder="enter text here..."
+                  value={form.presentParticiple}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="my-3">
+                <label className="text-sm capitalize text-primary">past</label>
+                <br />
+                <input
+                  name="past"
+                  type="text"
+                  className="focus:shadow-outline-primary block w-full rounded border border-primary bg-base-100 p-2 capitalize focus:border-primary-focus focus:outline-none"
+                  placeholder="enter text here..."
+                  value={form.past}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="my-3">
+                <label className="text-sm capitalize text-primary">
+                  past participle
+                </label>
+                <br />
+                <input
+                  name="pastParticiple"
+                  type="text"
+                  className="focus:shadow-outline-primary block w-full rounded border border-primary bg-base-100 p-2 capitalize focus:border-primary-focus focus:outline-none"
+                  placeholder="enter text here..."
+                  value={form.pastParticiple}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="mt-4 flex justify-end">
+                <button
+                  disabled={isLoading}
+                  className="rounded-lg bg-secondary p-2 text-sm font-medium capitalize text-secondary-content w-32 mt-4"
+                  type="submit"
+                >
+                  {isLoading ? 'Loading...' : 'Submit'}
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
